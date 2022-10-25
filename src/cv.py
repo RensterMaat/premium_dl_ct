d421697a25767d5a2fcfef7194633d05757d7725
rens <rens.termaat@gmail.com>
2022-10-25 14:14:38 +0200

    created datapreprocessing pipeline

---
+++ src/cv.py
@@ -0,0 +1,14 @@
+# https://www.kaggle.com/code/ayuraj/use-pytorch-lightning-with-weights-and-biases/notebook
+# https://www.paepper.com/blog/posts/hyperparameter-tuning-on-numerai-data-with-pytorch-lightning-and-wandb/
+
+
+def train(model, datamodule, config):
+    pass
+
+
+def infer(model, datamodule):
+    pass
+
+
+def leave_one_center_out_cross_validate(model, datamodule):
+    pass
---
+++ src/data.py
@@ -0,0 +1,15 @@
+class Dataset:
+    def __init__(
+        self,
+        paths_to_center_folders,
+        prediction_target_file_path,
+        target,
+        train_transform,
+        val_transform,
+    ):
+        pass
+
+
+class DataModule:
+    def __init__(self, dataset, test_center):
+        pass
