0660ad3837432a9bcd74715d0f4012a9177fbf8e
rens <rens.termaat@gmail.com>
2022-10-27 07:51:44 +0200

    basic model

--- src/cv.py
+++ src/cv.py
@@ -1,9 +1,30 @@
-# https://www.kaggle.com/code/ayuraj/use-pytorch-lightning-with-weights-and-biases/notebook
-# https://www.paepper.com/blog/posts/hyperparameter-tuning-on-numerai-data-with-pytorch-lightning-and-wandb/
+import wandb
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning import Trainer, seed_everything
+from model import Model
 
 
 def train(model, datamodule, config):
-    pass
+    model = Model()
+
+    seed_everything(0)
+
+    logger = WandbLogger(
+        name='hello1',
+        project='project_skeleton_on_lung_lesions',
+    )
+
+    trainer = Trainer(
+        max_epochs=3,
+        gpus=1,
+        deterministic=True,
+        fast_dev_run=False,
+        logger=logger
+    )
+            
+    trainer.fit(model, dm)
+
+    wandb.finish()
 
 
 def infer(model, datamodule):
--- src/model.py
+++ src/model.py
@@ -1,6 +1,52 @@
-# https://www.kaggle.com/code/ayuraj/use-pytorch-lightning-with-weights-and-biases/notebook
+import torch
+import torch.nn as nn
+import wandb
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning import LightningModule, Trainer, seed_everything
+from monai.networks.nets.densenet import DenseNet121
 
 
-class LightningModule:
-    def __init__(self, input_dimensionality, pretrained):
-        pass
+class Model(LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.model = DenseNet121(spatial_dims=3, in_channels=1, out_channels=1)
+
+    def forward(self, x):
+        return self.model(x)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(self.model.parameters(), lr=1e-2)
+
+        return optimizer
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch["img"], batch["label"]
+        y_hat = torch.sigmoid(self.model(x))
+
+        loss = nn.BCELoss()(y_hat.squeeze(), y.float())
+
+        self.log_dict(
+            {"train_loss": loss},
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+        )
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch["img"], batch["label"]
+        y_hat = torch.sigmoid(self.model(x))
+
+        loss = nn.BCELoss()(y_hat.squeeze(), y.float())
+
+        self.log_dict(
+            {"valid_loss": loss},
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+        )
+
+        return loss
