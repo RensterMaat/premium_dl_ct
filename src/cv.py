ccba293b2b97792eefee6d671e0b2c2ffd167bba
rens <rens.termaat@gmail.com>
2022-10-27 17:25:57 +0200

    added auc metric

--- src/cv.py
+++ src/cv.py
@@ -2,29 +2,36 @@ import wandb
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning import Trainer, seed_everything
 from model import Model
+from data import DataModule
 
 
 def train(model, datamodule, config):
-    model = Model()
-
     seed_everything(0)
 
     logger = WandbLogger(
-        name='hello1',
-        project='project_skeleton_on_lung_lesions',
+        name="hello1",
+        project="project_skeleton_on_lung_lesions",
     )
 
     trainer = Trainer(
-        max_epochs=3,
-        gpus=1,
-        deterministic=True,
-        fast_dev_run=False,
-        logger=logger
+        max_epochs=3, gpus=1, deterministic=True, fast_dev_run=False, logger=logger
     )
-            
-    trainer.fit(model, dm)
+
+    trainer.fit(model, datamodule)
 
     wandb.finish()
+
+
+dm = DataModule(
+    r"C:\Users\user\data\dl_radiomics\preprocessed_3d",
+    r"C:\Users\user\data\tables\lesion_followup_curated_v4.csv",
+    "lung",
+    "mst",
+)
+
+model = Model()
+
+train(model, dm, None)
 
 
 def infer(model, datamodule):
--- src/model.py
+++ src/model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import wandb
+from torchmetrics.classification import BinaryAUROC
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning import LightningModule, Trainer, seed_everything
 from monai.networks.nets.densenet import DenseNet121
@@ -10,6 +11,8 @@ class Model(LightningModule):
     def __init__(self):
         super().__init__()
         self.model = DenseNet121(spatial_dims=3, in_channels=1, out_channels=1)
+        self.train_auc = BinaryAUROC(pos_label=1)
+        self.val_auc = BinaryAUROC(pos_label=1)
 
     def forward(self, x):
         return self.model(x)
@@ -24,9 +27,10 @@ class Model(LightningModule):
         y_hat = torch.sigmoid(self.model(x))
 
         loss = nn.BCELoss()(y_hat.squeeze(), y.float())
+        self.train_auc.update(y_hat.squeeze(), y.int())
 
         self.log_dict(
-            {"train_loss": loss},
+            {"train_loss": loss, "train_auc": self.train_auc.compute()},
             on_step=False,
             on_epoch=True,
             prog_bar=True,
@@ -40,9 +44,10 @@ class Model(LightningModule):
         y_hat = torch.sigmoid(self.model(x))
 
         loss = nn.BCELoss()(y_hat.squeeze(), y.float())
+        self.val_auc.update(y_hat.squeeze(), y.int())
 
         self.log_dict(
-            {"valid_loss": loss},
+            {"valid_loss": loss, "valid_auc": self.val_auc.compute()},
             on_step=False,
             on_epoch=True,
             prog_bar=True,
