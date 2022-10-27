3aa5995d424cd5317d01e155326ad7f7d7bd82ec
rens <rens.termaat@gmail.com>
2022-10-27 09:35:24 +0200

    implemented per patient sampler

--- README.md
+++ README.md
@@ -4,15 +4,13 @@ Deep learning on CT imaging to predict response to checkpoint inhibitors in mela
 
 # todo
 - adapt dataset to allow patient level classification tasks
-    - add patient id to batch
-    - make sampler to add patients to batch until batch is full
+    - make train-val split on patient level 
     - only filter on missing lesion labels in train dataset, not in val and test
-    - add patient outcome to dataset, but also to datamodule? how to do this neatly?
+    - add patient outcome to model
 - lesion and patient level auc as metric
 - data augmentation 
 - cosine annealing and early stopping callbacks
 - implement 2.5d preprocessing
-    - 
 - implement different scales in preprocessing (both zoom and crop)
 - make model dependent on config dictionary
 - random search over good initial parameters for model
--- src/data.py
+++ src/data.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import itertools
+import numpy as np
+import random
+from collections import defaultdict
+from torch.utils.data import Sampler
 from pathlib import Path
 from monai.data import CacheDataset
 from pytorch_lightning import LightningDataModule
@@ -61,22 +65,84 @@ class DataModule(LightningDataModule):
 
     def data_dir_to_dict(self, dir):
         return [
-            {"img": str(lesion_path), "label": self.target.loc[lesion_path.name]}
+            {
+                "img": str(lesion_path),
+                "label": self.target.loc[lesion_path.name],
+                "patient": lesion_path.name.split(".")[0][:-2],
+            }
             for lesion_path in dir.iterdir()
             if lesion_path.name in self.target.index
         ]
 
     def train_dataloader(self):
         return DataLoader(
-            self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=1
+            self.train_dataset,
+            batch_sampler=GroupedSampler(
+                groups=[x["patient"] for x in self.train_dataset],
+                shuffle=True,
+                max_batch_size=16,
+            ),
         )
 
     def val_dataloader(self):
         return DataLoader(
-            self.val_dataset, batch_size=self.batch_size, shuffle=False, num_workers=1
+            self.val_dataset,
+            batch_sampler=GroupedSampler(
+                groups=[x["patient"] for x in self.val_dataset],
+                shuffle=False,
+                max_batch_size=16,
+            ),
         )
 
     def test_dataloader(self):
         return DataLoader(
-            self.test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=1
+            self.test_dataset,
+            batch_sampler=GroupedSampler(
+                groups=[x["patient"] for x in self.test_dataset],
+                shuffle=False,
+                max_batch_size=16,
+            ),
         )
+
+
+class GroupedSampler(Sampler):
+    def __init__(self, groups, max_batch_size, shuffle=False):
+        self.max_batch_size = max_batch_size
+        self.shuffle = shuffle
+
+        self.unique_groups = np.unique(groups)
+
+        self.indices_per_group = defaultdict(list)
+        self.count_per_group = defaultdict(int)
+
+        for ix, group in enumerate(groups):
+            self.indices_per_group[group].append(ix)
+            self.count_per_group[group] += 1
+
+        self.make_batches_from_groups()
+
+    def make_batches_from_groups(self):
+        self.all_batches = []
+        current_batch = []
+        current_batch_length = 0
+        for group in self.unique_groups:
+            group_length = self.count_per_group[group]
+            if current_batch_length + group_length > self.max_batch_size:
+                self.all_batches.append(current_batch)
+                current_batch = self.indices_per_group[group].copy()
+                current_batch_length = group_length
+            else:
+                current_batch.extend(self.indices_per_group[group])
+                current_batch_length += group_length
+        if current_batch:
+            self.all_batches.append(current_batch)
+
+    def __iter__(self):
+        if self.shuffle:
+            random.shuffle(self.all_batches)
+
+        for batch in self.all_batches:
+            yield batch
+
+    def __len__(self):
+        return len(self.all_batches)
